9c286e1a8738d014a3585b1dbf81a2f17ed86810
jasppankaj <jasp.pankaj@gmail.com>
2025-07-13 23:41:07 +0530

    Calc Updated

--- calc.cpp
+++ calc.cpp
@@ -1,94 +1,86 @@
 #include <iostream>
 using namespace std;
-  
 
-int main(){
-    float result=0;
-    cout<<"Welcome to the Calculator"<<endl;
-    float  num,num2 ;
-    bool condition;
-    do{
-        if(result==0){
-            cout<<"Enter Number : ";
-        cin>>num;
-        result=num;
-        cout<<"Result :"<<result<<endl;
+int main()
+{
+    float result = 0;
+    cout << "Welcome to the Calculator" << endl;
+    float num, num2;
+    while (true)
+    {
 
-        }else
+        if (result == 0)
         {
-            cout<<"Result :"<<result<<endl;
+            cout << "Enter Number : ";
+            cin >> num;
+            result = num;
+            cout << "You entered :" << result << endl;
         }
-        
-        
 
-        
-        cout<<" + for Addition       "<<"\t - for Subtraction"<<endl;
-        cout<<" * for Multiplication "<<"\t / for Division"<<endl;
-        cout<<" c for Clear          "<<"\t e for Exit"<<endl;
+        cout << "***********************************************" << endl;
+        cout << " + for Addition       " << "\t - for Subtraction" << endl;
+        cout << " * for Multiplication " << "\t / for Division" << endl;
+        cout << " c for Clear          " << "\t e for Exit" << endl;
+
+        cout << "***********************************************" << endl;
 
         string user_choice;
-        cout<<"Enter your choice : ";
-        cin>>user_choice;
+        cout << "Enter your choice : ";
+        cin >> user_choice;
 
-        
-
-        if(user_choice == "+"){
-            cout<<"Enter a number : ";
-            cin>>num;
-          condition = true;
-          
-          result = result + num ;
-          cout<<"Result is : "<<result<<endl;  
-        }
-
-        else if(user_choice == "-"){
-            cout<<"Enter a number : ";
-        cin>>num;
-            condition = true;
-            result = result - num;
-            cout<<"Result is : "<<result<<endl;
-        }
-
-        else if(user_choice == "*"){
-            cout<<"Enter a number : ";
-        cin>>num;
-            condition = true;
-            result = result * num;
-            cout<<"Result is : "<<result<<endl;
-        }
-
-        else if(user_choice == "/"){
-            cout<<"Enter a number : ";
-        cin>>num;
-            condition = true;
-            if(num != 0){
-                result = result / num;
-                cout<<"Result is : "<<result<<endl;
+        if (user_choice == "+" || user_choice == "-" || user_choice == "*" || user_choice == "/")
+        {
+            cout << "Enter a number : ";
+            cin >> num;
+            if (user_choice == "+")
+            {
+                cout<<result<<" + "<<num<<" = ";
+                result = result + num;
+                cout << result << endl;
             }
-            else{
-                
-                cout<<"Invalid"<<endl;
+            if (user_choice == "-")
+            {
+                cout<<result<<" - "<<num<<" = ";
+                result = result - num;
+                cout << result << endl;
+            }
+            if (user_choice == "*")
+            {
+                cout<<result<<" * "<<num<<" = ";
+                result = result * num;
+                cout << result << endl;
+            }
+            if (user_choice == "/")
+            {
+                if (num != 0)
+                {
+                    cout<<result<<" / "<<num<<" = ";
+                    result = result / num;
+                    cout << result << endl;
+                }
+                else
+                {
+                    cout << "Invalid" << endl;
+                }
             }
         }
-
-        else if(user_choice == "c" || user_choice == "C"){
-            condition = true;
-            result = 0;
-            cout<<"Result After Clear : "<<result<<endl;
+        else
+        {
+            if (user_choice == "c" || user_choice == "C")
+            {
+                result = 0;
+                cout << "Cleared, Start Again" << endl<<endl;
+            }
+            else if (user_choice == "e" || user_choice == "E")
+            {
+                break;
+            }
+            else
+            {
+                cout << "Invalid Input , Choose from the following" << endl;
+            }
         }
-        else if(user_choice == "e" || user_choice == "E"){
-            condition = false;
-            break;
-        }
-
-        else{
-            condition = true;
-            cout<<"Invalid input , choose from the above."<<endl;
-        }
-
-        
     }
-    while(condition);
 
-    cout<<"Thank you for using."<<endl;
+    cout << "Thank you for using." << endl;
 }
